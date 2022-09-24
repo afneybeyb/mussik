af412135a538158480a7df610a98efd413471543
afneybeyb <afneybeyb@gmail.com>
2022-09-24 21:21:38 +0200

    created PlayPause component showing play or pause icon depending on state

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,20 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ song, activeSong, isPlaying, handlePause, handlePlay }) => {
+	return (isPlaying && activeSong?.title === song.title ? (
+		<FaPauseCircle
+			className="text-gray-200"
+			size={35}
+			onClick={handlePause}
+		/>
+	) : (
+		<FaPlayCircle
+			className="text-gray-200"
+			size={35}
+			onClick={handlePlay}
+		/>
+	));
+};
 
 export default PlayPause;
+
