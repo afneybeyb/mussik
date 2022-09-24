94da1dd43129028ddbf771140170715434f12e2a
afneybeyb <afneybeyb@gmail.com>
2022-09-24 12:18:52 +0200

    created and exported API request function

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Creation of Shazam Core API request
+export const shazamCoreApi = createApi({
+	reducerPath: "shazamCoreApi",
+	baseQuery: fetchBaseQuery({
+		baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+
+		// Adding API key from .env to the request's headers
+		prepareHeaders: (headers) => {
+			headers.set("X-RapidAPI-Key", import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+			return headers;
+		}
+	}),
+	// Define API endpoints
+	endpoints: (builder) => ({
+		getTopCharts: builder.query({ query: () => "/charts/world" }),
+	})
+});
+
+// Export API endpoints
+export const {
+	useGetTopChartsQuery,
+} = shazamCoreApi;
+
