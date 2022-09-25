6f9978ec3c1ac6d3c66c6be949f4c3abbfc844aa
afneybeyb <afneybeyb@gmail.com>
2022-09-25 14:12:07 +0200

    added top margin on Loader + Eroor component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Error = ({ title }) => (
-	<div className="w-full flex justify-center items-center flex-col text-center">
-		<h1 className="mt-36 text-2xl font-bold">{title || "An error occured"}</h1>
+	<div className="mt-36 w-full flex justify-center items-center flex-col text-center">
+		<h1 className="text-2xl font-bold">{title || "An error occured"}</h1>
 		<p>Please try it again later</p>
 	</div>
 );
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 import { loader } from '../assets';
 
 const Loader = ({ title }) => (
-	<div className="w-full flex justify-center items-center flex-col text-center">
+	<div className="mt-24 w-full flex justify-center items-center flex-col text-center">
 		<img className="w-32 h-32 object-contain" src={loader} alt="Loading icon" />
 		<h1 className="mt-2 text-2xl font-bold">{title || "Loading"}</h1>
 	</div>
