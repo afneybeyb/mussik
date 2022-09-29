75e48b44d1010f7947641f52f66b732cc18b89bb
afneybeyb <afneybeyb@gmail.com>
2022-09-29 20:38:09 +0200

    implementation of DetailsHeader

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,20 +1,26 @@
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
 
 const SongDetails = () => {
 	const { songid } = useParams();
+	const dispatch = useDispatch();
+	const { activeSong, isPlaying } = useSelector((state) => state.player);
 
 	const { data: songData, isFetching: isFetchingSongData } = useGetSongDetailsQuery({ songid });
 
 	return (
-		<div className="flex flex-col">
-			<div className="mb-10">
-				<h2 className="mt-4 mb-2 text-3xl font-bold text-my-yellow">Lyrics:</h2>
+		<div className="my-10 mx-0 md:mx-4 flex flex-col gap-6">
+			<DetailsHeader artistId="" songData={songData} />
+			<div className="mt-8">
+				<h2 className="my-2 text-3xl font-bold text-my-yellow">Lyrics:</h2>
 				<div className="mt-5">
 					{songData?.sections[1].type === "LYRICS" ?
 						songData?.sections[1].text.map((songDetail, i) => (
-							<p className="my-2">{songDetail}</p>
+							<p className="my-2" key={i}>{songDetail}</p>
 						)) :
 						isFetchingSongData ? <p>Loading lyrics...</p> : <p>Sorry, no lyrics found.</p>
 					}
