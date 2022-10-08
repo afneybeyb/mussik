c0842b14d7ef58b0d5311ed6f3e34ca7a7ec0a39
afneybeyb <afneybeyb@gmail.com>
2022-10-08 20:36:26 +0200

    moved activeSong, isPlaying and handlePlay/Pause functions directly to target cmpt

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,17 +1,31 @@
-import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 
-const PlayPause = ({ song, activeSong, isPlaying, handlePause, handlePlay }) => {
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+
+const PlayPause = ({ song, data }) => {
+	const dispatch = useDispatch();
+	const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+	const handlePauseClick = () => {
+		dispatch(playPause(false));
+	}
+	const handlePlayClick = (song, data) => {
+		dispatch(setActiveSong({ song, data }));
+		dispatch(playPause(true));
+	}
+
 	return (isPlaying && activeSong?.title === song.title ? (
 		<FaPauseCircle
-			className="text-gray-200"
+			className="text-gray-200 cursor-pointer hover:text-my-yellow"
 			size={35}
-			onClick={handlePause}
+			onClick={handlePauseClick}
 		/>
 	) : (
 		<FaPlayCircle
-			className="text-gray-200"
+			className="text-gray-200 cursor-pointer hover:text-my-yellow"
 			size={35}
-			onClick={handlePlay}
+			onClick={() => handlePlayClick(song, data)}
 		/>
 	));
 };
