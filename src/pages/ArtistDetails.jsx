09b2e49f39f7b1bcbcb3093d0dc16deeec1f50b4
afneybeyb <afneybeyb@gmail.com>
2022-10-23 20:17:13 +0200

    created page similar to SongDetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,27 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import { useParams } from 'react-router-dom';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
+
+const ArtistDetails = () => {
+	const { id: artistId } = useParams();
+
+	const { data: artistData, isFetching: isFetchingArtistData, errorArtistData } = useGetArtistDetailsQuery({ artistId });
+
+	if (isFetchingArtistData) return <Loader title="Searching for artist details" />
+	if (errorArtistData) return <Error />
+
+	return (
+		<div className="my-10 mx-0 md:mx-4 flex flex-col gap-6">
+			<DetailsHeader artistId={artistId} artistData={artistData} />
+			<button onClick={() => console.log(artistData?.songs)}>Show me</button>
+			<RelatedSongs
+				data={Object.values(artistData?.songs)}
+				artistId={artistId}
+			/>
+		</div>
+	);
+};
 
 export default ArtistDetails;
+
